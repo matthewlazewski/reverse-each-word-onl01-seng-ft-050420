9f3dda81f8b7819cba6ff63cdd5ca11f56b276ad
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-06 00:16:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence1)
+  sentence2 = []
   sentence1.split(",")
   sentence1.each do |word|
-    word.reverse
-    sentence1
+    sentence2 << word.reverse
   end 
 end 
   
